3479894d4d487a6250a6879a921c035a7e2d689c
Nilan <nilanhansajith7@gmail.com>
2024-04-13 01:08:18 +0530

    delete error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React from 'react';
+import BookingPage from './pages/BookingPage';
+import Reservations from './pages/Reservations';
+import UpdateDetails from './pages/UpdateDetails';
+
+ 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+        <Routes>
+          
+          <Route path="/" element={<BookingPage />}></Route>
+          <Route path="/reservations" element={<Reservations />}></Route>
+          <Route path="/updatedetails/:id" element={<UpdateDetails />}></Route>
+        </Routes>
+        </div>
+      </BrowserRouter>
+    
   );
 }
 
